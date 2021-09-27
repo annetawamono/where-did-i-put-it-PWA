1ee51140bf4fd307436234931af321a06f2ab16d
Anneta Wamono <annetawamono@yahoo.com.au>
2021-09-27 18:25:29 +0200

    Added PWA

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import { openDB } from 'idb';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    name: 'Where did I put it App',
+    themeColor: '#1B264F',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
 }
